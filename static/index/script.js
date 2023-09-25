e90ff12fdafc8e40d559bb9b7a554c4403d8e3ef
Lungeanu Razvan <razvanlungeanu21@gmail.com>
2023-09-25 19:38:53 +0300

    wip footer

--- static/index/script.js
+++ static/index/script.js
@@ -4,10 +4,24 @@ const loadNavbar = () => {
   xhr.onreadystatechange = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
       navbarContainer.innerHTML = xhr.responseText;
+      loadFooter();
     }
   };
 
   xhr.open("GET", "navbar.html", true);
+  xhr.send();
+};
+
+const loadFooter = () => {
+  const footerContainer = document.getElementById("footer-container");
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      footerContainer.innerHTML = xhr.responseText;
+    }
+  };
+
+  xhr.open("GET", "footer.html", true);
   xhr.send();
 };
 
